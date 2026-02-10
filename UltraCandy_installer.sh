e1753bed8ed3b6c798f384019073b231debae629
CANDY <mirukaiano@gmail.com>
2026-02-10 23:21:33 +0300

    Update UltraCandy_installer.sh

--- UltraCandy_installer.sh
+++ UltraCandy_installer.sh
@@ -696,7 +696,7 @@ alias mkdir="mkdir -pv"
 alias wget="wget -c"
 
 # Git aliases
-alias g="git clone"
+alias g="git clone --depth 1"
 alias ga="git add ."
 alias gc="git commit -m"
 alias gp="git push"
@@ -890,7 +890,7 @@ alias mkdir="mkdir -pv"
 alias wget="wget -c"
 
 # Git aliases
-alias g="git clone"
+alias g="git clone --depth 1"
 alias ga="git add ."
 alias gc="git commit -m"
 alias gp="git push"
