976bf0495fee81b799445f909322b5357f213f7d
CANDY <mirukaiano@gmail.com>
2026-02-11 00:55:34 +0300

    Update Update_UltraCandy.sh

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -13181,12 +13181,8 @@ prompt_logout() {
             echo "✅ Starting chosen bar (re-login post update is advised)..."
             sleep 5
             if [ "$PANEL_CHOICE" = "waybar" ]; then
-                qs -c overview >/dev/null 2>&1 &
-                sleep 0.5
                 rm -rf "$HOME/ultracandyinstall" && systemctl --user stop hyprpanel.service &>/dev/null && systemctl --user restart waybar.service &>/dev/null
             else
-                qs -c overview >/dev/null 2>&1 &
-                sleep 0.5
                 rm -rf "$HOME/ultracandyinstall" && systemctl --user stop waybar.service &>/dev/null && systemctl --user restart hyprpanel.service &>/dev/null
             fi
             ;;
