3d20257a9e95d009eccea937d67db729a9a042e1
CANDY <mirukaiano@gmail.com>
2026-02-11 00:43:16 +0300

    Update Update_UltraCandy.sh

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -13181,16 +13181,18 @@ prompt_logout() {
             echo "✅ Starting chosen bar (re-login post update is advised)..."
             sleep 5
             if [ "$PANEL_CHOICE" = "waybar" ]; then
-                systemctl --user stop hyprpanel.service &>/dev/null
-                rm -rf "$HOME/ultracandyinstall" && systemctl --user restart waybar.service &>/dev/null
+                qs -c overview >/dev/null 2>&1 &
+                sleep 0.5
+                rm -rf "$HOME/ultracandyinstall" && systemctl --user stop hyprpanel.service &>/dev/null && systemctl --user restart waybar.service &>/dev/null
             else
-                systemctl --user stop waybar.service &>/dev/null
-                rm -rf "$HOME/ultracandyinstall" && systemctl --user restart hyprpanel.service &>/dev/null
+                qs -c overview >/dev/null 2>&1 &
+                sleep 0.5
+                rm -rf "$HOME/ultracandyinstall" && systemctl --user stop waybar.service &>/dev/null && systemctl --user restart hyprpanel.service &>/dev/null
             fi
             ;;
         *)
             print_status "Logging out..."
-            hyprctl dispatch exit && rm -rf "$HOME/ultracandyinstall"
+            rm -rf "$HOME/ultracandyinstall" && hyprctl dispatch exit
             ;;
     esac
 }
