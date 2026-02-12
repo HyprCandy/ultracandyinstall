3daaabaeffee078658d6d831e967fa462458ee74
CANDY <mirukaiano@gmail.com>
2026-02-13 00:14:13 +0300

    Update Update_UltraCandy.sh

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -3654,7 +3654,7 @@ setup_custom_config() {
 
 #[IMPORTANT]#
 # Add custom settings at the very end of the file.
-# To reset, delete the 'hyprcustom' folder (not just the 'custom.conf' file) before rerunning the script to regenerate the default setup.
+# This "hypr" folder is backed up on updates so you can copy you "userprefs" from the hyprviz.conf backup to the new file
 #[IMPORTANT]#
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -4434,7 +4434,7 @@ else
 
 #[IMPORTANT]#
 # Add custom settings at the very end of the file.
-# To reset, delete the 'hyprcustom' folder (not just the 'custom.conf' file) before rerunning the script to regenerate the default setup.
+# This "hypr" folder is backed up on updates so you can copy you "userprefs" from the hyprviz.conf backup to the new file
 #[IMPORTANT]#
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
