cc895739954380bc764f688db22c1b43f6f6adf2
CANDY <mirukaiano@gmail.com>
2026-02-26 19:23:09 +0300

    Remove duplicate GTK_THEME entries
    Removed duplicate GTK_THEME environment variable settings.

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -5488,7 +5488,6 @@ env = XDG_CURRENT_DESKTOP,Hyprland
 env = XDG_SESSION_TYPE,wayland
 env = XDG_SESSION_DESKTOP,Hyprland
 # GTK
-env = GTK_USE_PORTAL,1
 env = GTK_THEME,adw-gtk3-dark
 # QT
 env = QT_QPA_PLATFORM,wayland
@@ -5508,7 +5507,6 @@ env = OZONE_PLATFORM,wayland
 env = ELECTRON_OZONE_PLATFORM_HINT,wayland
 # Extra
 env = WINIT_UNIX_BACKEND,wayland
-env = GTK_THEME,adw-gtk3-dark
 env = WLR_DRM_NO_ATOMIC,1
 env = WLR_NO_HARDWARE_CURSORS,1
 # Virtual machine display scaling
@@ -6142,7 +6140,6 @@ env = XDG_CURRENT_DESKTOP,Hyprland
 env = XDG_SESSION_TYPE,wayland
 env = XDG_SESSION_DESKTOP,Hyprland
 # GTK
-env = GTK_USE_PORTAL,1
 env = GTK_THEME,adw-gtk3-dark
 # QT
 env = QT_QPA_PLATFORM,wayland
@@ -6162,7 +6159,6 @@ env = OZONE_PLATFORM,wayland
 env = ELECTRON_OZONE_PLATFORM_HINT,wayland
 # Extra
 env = WINIT_UNIX_BACKEND,wayland
-env = GTK_THEME,adw-gtk3-dark
 env = WLR_DRM_NO_ATOMIC,1
 env = WLR_NO_HARDWARE_CURSORS,1
 # Virtual machine display scaling
