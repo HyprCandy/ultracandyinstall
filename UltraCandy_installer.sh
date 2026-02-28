d3db59fe8d892b65e5205b8af7ebc3ea072f81d8
CANDY <mirukaiano@gmail.com>
2026-03-01 01:33:51 +0300

    Update UltraCandy_installer.sh

--- UltraCandy_installer.sh
+++ UltraCandy_installer.sh
@@ -4148,6 +4148,8 @@ watch_gtk_file() {
         theme=$(extract_cursor_theme "$file")
         size=$(extract_cursor_size "$file")
         update_hypr_cursor_env "$theme" "$size"
+        sleep 0.5
+        systemctl --user restart cursor-theme-watcher.service
     done
 }
 
