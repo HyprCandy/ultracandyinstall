791c557bd76015bf2da9faf3006046b760dcdeb6
CANDY <mirukaiano@gmail.com>
2026-03-02 01:07:06 +0300

    Update Update_UltraCandy.sh

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -7135,7 +7135,7 @@ bind = Alt, H, exec, hyprctl hyprsunset gamma -10 #Reduce gamma by 10%
 
 #### Actions ####
 
-bind = CTRL, G, exec, $HYPRSCRIPTS/gamemode.sh						  #Toggle game-mode
+bind = ALT, G, exec, $HYPRSCRIPTS/gamemode.sh						  #Toggle game-mode
 #bind = $mainMod, M, exec, ~/.config/hypr/scripts/power.sh exit 				  #Logout
 #bind = $mainMod,SPACE, hyprexpo:expo, toggle						  #Hyprexpo-plus workspaces overview
 bind = $mainMod SHIFT, R, exec, $HYPRSCRIPTS/loadconfig.sh                                 #Reload Hyprland configuration
@@ -7359,7 +7359,7 @@ bind = Alt, H, exec, hyprctl hyprsunset gamma -10 #Reduce gamma by 10%
 
 #### Actions ####
 
-bind = CTRL, G, exec, $HYPRSCRIPTS/gamemode.sh						  #Toggle game-mode
+bind = ALT, G, exec, $HYPRSCRIPTS/gamemode.sh						  #Toggle game-mode
 #bind = $mainMod, M, exec, ~/.config/hypr/scripts/power.sh exit 				  #Logout
 #bind = $mainMod,SPACE, hyprexpo:expo, toggle						  #Hyprexpo-plus workspaces overview
 bind = $mainMod SHIFT, R, exec, $HYPRSCRIPTS/loadconfig.sh                                 #Reload Hyprland configuration
