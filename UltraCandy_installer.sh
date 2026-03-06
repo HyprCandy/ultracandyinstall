3223625ed1fcc59f45e35c3e39cf4d1b004c321a
CANDY <mirukaiano@gmail.com>
2026-03-06 19:19:13 +0300

    Update UltraCandy_installer.sh

--- UltraCandy_installer.sh
+++ UltraCandy_installer.sh
@@ -1072,7 +1072,7 @@ setup_ultracandy() {
     config_dirs=(".config" ".icons" ".ultracandy-zsh.zsh")
 
     # Add files/folders to exclude from deletion
-    preserve_items=("GJS" "Candy" )
+    preserve_items=("GJS" "Candy" ".git")
 
     if [ ${#config_dirs[@]} -eq 0 ]; then
         echo "❌ No configuration directories specified."
